4edb39725741e27b4efda99e2f8c9b34f44e3893
Matt Books <mattbooks@squareup.com>
2021-06-03 16:58:24 -0700

    Instrument errors

--- lib/square/http/instrumentation.rb
+++ lib/square/http/instrumentation.rb
@@ -1,7 +1,6 @@
 module Square
   class Instrumentation < ::Faraday::Middleware
     attr_reader :instrumenter
-    API_SERVICE_PATTERN = %r[/v2/([^/]+)/.*]
 
     def initialize(app, instrumenter: ActiveSupport::Notifications)
       super(app)
@@ -22,6 +21,17 @@ module Square
           ended_at: ended,
         })
       end
+    rescue Faraday::Error => e
+      instrumenter.instrument('square.api_request_error', {
+        request_env: request_env,
+        response_status: e.response_status,
+        error_class: e.class.to_s,
+        api_name: request_env.request.context&.fetch(:api_name, nil),
+        started_at: started,
+        ended_at: Time.now,
+      })
+
+      raise
     end
   end
 end
